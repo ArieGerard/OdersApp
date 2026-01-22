6d0469e4e7863c1832b465364b34bcbc2db21832
ObadiahF <obadiahfusco@icloud.com>
2026-01-22 11:40:24 -0700

    Add UsersRepository

--- src/main/java/com/gcu/activity1/data/UsersRepository.java
+++ src/main/java/com/gcu/activity1/data/UsersRepository.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+// Comment for commit since i already did this
 @Repository
 public interface UsersRepository extends CrudRepository<UserEntity, Integer> {
 
